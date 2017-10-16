01f051af3a10e3469f4130857d1395f8fab5de3f
Rich Mama <postmaster.richmama@gmail.com>
2017-10-16 23:04:46 +0100

    Updated web routes with original routes

--- app/Helpers/Contracts/HelperContract.php
+++ app/Helpers/Contracts/HelperContract.php
@@ -4,7 +4,6 @@ namespace App\Helpers\Contracts;
 Interface HelperContract
 {
         public function sendEmail($to,$subject,$data,$view,$type);
-        public function getUploads();
-        public function getComments($p);
+        public function function getBlogPosts();
 }
  ?>
